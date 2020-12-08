1340090cbb649196554b84374d79b08c98220839
Jassiel <mijumarutv@gmail.com>
2020-12-07 23:36:12 -0600

    Pequeños ajustes a la caja de diálogo. Se agregaron todos los archivos de diálogo de misión del Nivel 1.

--- src/Levels/Level_1.gd
+++ src/Levels/Level_1.gd
@@ -2,8 +2,6 @@ extends Node
 onready var parent: Node2D
 onready var level: Node2D = get_node("LevelMap")
 onready var player: Player = get_node("LevelMap/Layer 3/Characters/Jugador")
+
 func _ready():
-	if SAVESYS.load_game(name):
-		player.queue_free()
-	else:
-		player.position = Vector2(0,0)
+	pass
